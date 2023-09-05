7c43a9ce9fc21d6de9c5c5eca573d9542217c132
aswin.as-at-124257330460 <vickyviji46@gmail.com>
2023-09-05 07:49:01 +0530

    added three features

---
+++ lib/features/delivery/presentation/cubit/delivery_state.dart
@@ -0,0 +1,10 @@
+part of 'delivery_cubit.dart';
+
+abstract class DeliveryState extends Equatable {
+  const DeliveryState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class DeliveryInitial extends DeliveryState {}
